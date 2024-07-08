4ba5c97a0f7e0120d81bf6d7d1fde92aec9dc45c
Thano <iliyanman2@gmail.com>
2024-07-08 23:03:30 +0300

    ni

--- empirevalue.py
+++ empirevalue.py
@@ -9,12 +9,9 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-<<<<<<< HEAD
-=======
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
->>>>>>> b5413a0 (amogus)
 
 @client.event
 async def on_ready():
@@ -36,11 +33,7 @@ async def sync(ctx):
 
 
 @tree.command(name="empirestats", description="get info about the stats of THE EMPIRE")
-<<<<<<< HEAD
-async def empirestats(ctx: discord.Interaction, api_key: str):
-=======
 async def empirestats(ctx: discord.Interaction):
->>>>>>> b5413a0 (amogus)
     embed = discord.Embed(
         title="THE EMPIRE BUSINESS"
     )
@@ -56,19 +49,12 @@ async def empirestats(ctx: discord.Interaction):
     WOOD_TYPES = ['ENCHANTED_ACACIA_LOG', 'ENCHANTED_BIRCH_LOG', 'ENCHANTED_DARK_OAK_LOG',
                   'ENCHANTED_JUNGLE_LOG', 'ENCHANTED_OAK_LOG', 'ENCHANTED_SPRUCE_LOG']
 
-<<<<<<< HEAD
-    for i in WOOD_TYPES:
-        info = get_prices(i)
-
-        embed.add_field(name=i, value=f"""
-=======
     WOOD_TYPES = sorted(WOOD_TYPES, key=lambda x: get_prices(x)["sell_offer"], reverse=True)
 
     for i in WOOD_TYPES:
         info = get_prices(i)
 
         embed.add_field(name=i.title().replace("_", " "), value=f"""
->>>>>>> b5413a0 (amogus)
                         Sell offer: {info["sell_offer"]}
                         Insta sell: {info["insta_sell"]}
                         Spread: {round(info["spread"], 1)}
@@ -88,20 +74,11 @@ async def isgigachad(ctx):
 async def empirevalue(ctx):
     await ctx.response.send_message("""
 Stock count: 10 000 000
-<<<<<<< HEAD
-Price per stock: 34 coins
-Total value: 340 000 000 coins
-=======
 Price per stock: 34 000 000 000 000 coins
 Total value: 340 000 000 000 000 000 000 coins
->>>>>>> b5413a0 (amogus)
 
 (updates once every 2 hours)
                                     """)
 
-<<<<<<< HEAD
-load_dotenv()
-=======
->>>>>>> b5413a0 (amogus)
 
 client.run(os.getenv('TOKEN'))
